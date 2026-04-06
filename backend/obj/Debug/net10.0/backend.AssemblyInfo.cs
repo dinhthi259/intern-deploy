c53968281a2b0f05077ae1d2a8b0606adf9e09fb
NguyenDinhThi <nguyendinhthi@loakeoaty.com>
2026-04-06 21:24:27 +0700

    fix jenkins pipeline

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1dc8d6495e117c5861d402dab9d83533db7243e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80798dfa212080c756f18915ead3a56f816f2ae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
